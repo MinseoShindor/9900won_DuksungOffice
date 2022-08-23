d708a59ee6ada3d38def74091f32c26e51eb65bc
tlsalstj01@duksung.ac.kr <tlsalstj01@duksung.ac.kr>
2022-08-24 05:34:00 +0900

    0824 minseo

--- app/src/main/java/com/example/a9900won_hackathon_duksung_postoffice/LoginActivity.java
+++ app/src/main/java/com/example/a9900won_hackathon_duksung_postoffice/LoginActivity.java
@@ -1,12 +1,13 @@
 package com.example.a9900won_hackathon_duksung_postoffice;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.os.Build;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
-import android.view.WindowManager;
-
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,5 +15,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button ExLoginBtn = findViewById(R.id.btn_loginBtn);
+        ExLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/a9900won_hackathon_duksung_postoffice/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.a9900won_hackathon_duksung_postoffice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView mListView = null;
+    BaseAdapterEx mAdapter = null;
+    ArrayList<List> mData = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mData = new ArrayList<List>();
+
+        for(int i = 0; i < 20; i++) {
+            List list = new List();
+
+            list.title = "길고양이를 학대하는 갤러리를 폐쇄하고 엄중한 수사를 해주십시오 " +i;
+//            list.context = "청원내용" + i;
+            list.writer = "이현민" + i;
+            list.date = "2022-08-23";
+
+            mData.add(list);
+
+        }
+
+        mAdapter = new BaseAdapterEx(this, mData);
+
+        mListView = (ListView) findViewById(R.id.main_list_view);
+        mListView.setAdapter(mAdapter);
+
+    }
+}
